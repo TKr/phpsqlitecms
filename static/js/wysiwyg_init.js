fd339ff2e7d5bd7b6a55f0e02c629f1bd7eb764f
TKr <tkr.tkr.pl@gmail.com>
2015-02-24 14:23:35 +0100

    Update wysiwyg_init.js

--- static/js/wysiwyg_init.js
+++ static/js/wysiwyg_init.js
@@ -4,11 +4,11 @@ tinymce.init({
    statusbar: false,
    entity_encoding : "raw",
    plugins: [
-        "advlist autolink lists link image anchor code"
+        "advlist autolink lists link image anchor code textcolor"
     ],
    target_list:false,
     
-   toolbar: "undo redo | styleselect | bold italic | bullist numlist | link unlink | image | code",
+   toolbar: "undo redo | styleselect | bold italic | forecolor backcolor | bullist numlist | link unlink | image | code",
    content_css : "../static/css/wysiwyg.css"
 
 });
